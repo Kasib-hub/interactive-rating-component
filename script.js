d4e071541a0e764706b43a84ad224f9bdbdbe501
Kasib <kasibabdullah8@gmail.com>
2022-09-29 00:08:20 -0400

    began working on behaviour, css passable

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn_submit = document.querySelector('.btn')
+const container = document.querySelector('.container')
+
+btn_submit.addEventListener('click', () => {
+
+    // cleared the div using this line
+    
+
+    container.innerHTML = 'Thank You!'
+})
